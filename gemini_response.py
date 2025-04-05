78b8b1e1039d8c39a4cdaedeff9ab6cbad19a486
ZeeKotomori <zikriaf23@gmail.com>
2025-04-05 12:59:50 +0700

    initial commit

---
+++ gemini_response.py
@@ -0,0 +1,15 @@
+from google import genai
+import streamlit as st
+
+api_key = st.secrets["gemini"]["api_key"]
+client = genai.Client(
+    api_key = api_key,
+)
+
+def get_response(prompt):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=prompt,
+    )
+    print(response.text)
+    return response.text

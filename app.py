a89c9ede6e8ec42320e0e41857adb7829a252fd2
Mr G <zikriaf23@gmail.com>
2025-04-08 22:25:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from gemini_response import get_response
 
 def main():
-    st.title("Zikri Bot 🤖")
+    st.title("Maysha AI 🤖")
 
     # Initialize chat history
     if "messages" not in st.session_state:
